0ecbb3d9fa3caad0e93a68fc21395cf15f38db9c
FouHamster <alina63111@gmail.com>
2024-04-22 10:59:58 +0700

    add img

--- vue/js/main.js
+++ vue/js/main.js
@@ -4,7 +4,7 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
-        
+        image: "./assets/vmSocks-green-onWhite.jpg"
     }
 
  })
